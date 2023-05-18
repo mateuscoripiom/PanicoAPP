3b2c5ee439d3b7f1f349bb7b563a607f502e3876
Mateus Coripio <69567720+mateuscoripiom@users.noreply.github.com>
2023-05-18 09:29:01 -0300

    18.05

--- app/src/main/java/com/example/appscream/CadastroActivity.java
+++ app/src/main/java/com/example/appscream/CadastroActivity.java
@@ -2,7 +2,10 @@ package com.example.appscream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Button btnscreamcad = findViewById(R.id.btnscreamcad);
+
+        btnscreamcad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CadastroActivity.this, PersonagensActivity.class));
+            }
+        });
     }
 }
