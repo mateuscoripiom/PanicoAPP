6ad62bd1c35706a51b989a6307caedf4007983fe
Mateus Coripio <yasmimborgesdias@gmail.com>
2023-05-31 12:16:36 -0300

    31.05

---
+++ app/src/main/java/com/example/appscream/Adaptery.java
@@ -0,0 +1,67 @@
+package com.example.appscream;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class Adaptery extends RecyclerView.Adapter<Adaptery.MyViewHolder> {
+
+    private Context mContext;
+    private List<MovieModelClass> mData;
+
+    public Adaptery(Context mContext, List<MovieModelClass> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        v = inflater.inflate(R.layout.activity_filme, parent, false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.id.setText(mData.get(position).getId());
+        holder.name.setText(mData.get(position).getName());
+
+        Glide.with(mContext)
+                .load(mData.get(position).getImg())
+                .into(holder.img);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView id;
+        TextView name;
+        ImageView img;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            id = itemView.findViewById(R.id.txtidf);
+            name = itemView.findViewById(R.id.txtnomef);
+            img = itemView.findViewById(R.id.imgfilmef);
+        }
+    }
+}
--- app/src/main/java/com/example/appscream/HomeActivity.java
+++ app/src/main/java/com/example/appscream/HomeActivity.java
@@ -1,10 +1,18 @@
 package com.example.appscream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private static String JSON_URL = "https://api.themoviedb.org/3/collection/2602?api_key=c1266995f1e4319216d8181d18012e4f&language=pt-BR";
+
+    List<MovieModelClass> movieList;
+    RecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
